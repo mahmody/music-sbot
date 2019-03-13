84b1106a11370ef57cef2dadac36828271b16ac0
mahmody <42693632+mahmody@users.noreply.github.com>
2019-03-13 23:45:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
-const Discord = require("discord.js");
-const RichEmbed = require("discord.js");
-const ytdl = require("ytdl-core");
-const devs = ["" , "" , "" , "id"]
-const request = require("request");
-const convert = require("hh-mm-ss")
-const fs = require("fs");
-const getYouTubeID = require("get-youtube-id");
-const fetchVideoInfo = require("youtube-info");
-const simpleytapi = require('simple-youtube-api')
-const yt_api_key = "AIzaSyDoH9YxF0yi6ljyi2txYZHB10vXNUEP_2U"
+const Discord = require('discord.js');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
 const prefix = "m-";
 var guilds = {};
 /////////////////////////////////////////////////////////////////
